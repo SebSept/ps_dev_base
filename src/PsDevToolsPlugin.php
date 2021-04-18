b844a97cbd9c1e5607275b3523647d0d39e7fbc9
SebSept <sebastienmonterisi@yahoo.fr>
2021-04-18 15:59:00 +0200

    Composer installation : added a welcome message.

--- src/PsDevToolsPlugin.php
+++ src/PsDevToolsPlugin.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace SebSept\PsDevToolsPlugin;
 
 use Composer\Composer;
+use Composer\Installer\PackageEvent;
+use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\Capable;
 use Composer\Plugin\PluginInterface;
 
-class PsDevToolsPlugin implements PluginInterface, Capable
+class PsDevToolsPlugin implements PluginInterface, Capable, EventSubscriberInterface
 {
     public function activate(Composer $composer, IOInterface $io): void
     {
@@ -28,11 +30,26 @@ class PsDevToolsPlugin implements PluginInterface, Capable
         $io->info('https://github.com/SebSept/ps_dev_base');
     }
 
-
-    public function getCapabilities():array
+    public function getCapabilities(): array
     {
         return [
-           'Composer\Plugin\Capability\CommandProvider' => PsDevToolsCommandProvider::class
-       ];
+            'Composer\Plugin\Capability\CommandProvider' => PsDevToolsCommandProvider::class
+        ];
     }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'post-package-install' => 'hello',
+        ];
+    }
+
+    public function hello(PackageEvent $event)
+    {
+        $event->getIO()->write('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+        $event->getIO()->write('~~ <fg=magenta>Congratulation !PsDevTool is now installed</>. ~~');
+        $event->getIO()->write('~~ run <comment>composer psdt:hello</comment> to get started.     ~~');
+        $event->getIO()->write('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+    }
+
 }
