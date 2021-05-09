cb7e5a65810f88d80c56a1a9063057a6baec6643
SebSept <sebastienmonterisi@yahoo.fr>
2021-05-09 21:20:57 +0200

    Revert "first working version (gnu/linux - macOs)" - commit on PR
    This reverts commit c9990822

--- resources/precommit.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-# this file must be symlinked in .git/hooks with the name "pre-commit"
-# it fire composer script "pre-commit" which launch phpstan + phpcsfix
-#
-# install with `ln -s $(pwd)/precommit.sh .git/hooks/pre-commit`
-
-(cd "./" && echo "start pre-commit hook ..." & exec 'composer' 'run-script' 'pre-commit')
